5ce2b1127651dd09cb6f5e4454841e6c2189d40b
nohsangwoo <nsgr12@naver.com>
2022-01-17 19:06:11 +0900

    set basic css form sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -10,8 +10,17 @@ const Sidebar = () => {
   return (
     <div>
       <div>
-        <button>
-          <HomeIcon />
+        <button className="flex items-center space-x-2 hover:text-white">
+          <HomeIcon className="h5 w-5" />
+          <p>Home</p>
+        </button>
+        <button className="flex items-center space-x-2 hover:text-white">
+          <SearchIcon className="h5 w-5" />
+          <p>Search</p>
+        </button>
+        <button className="flex items-center space-x-2 hover:text-white">
+          <LibraryIcon className="h5 w-5" />
+          <p>Your Library</p>
         </button>
       </div>
     </div>
