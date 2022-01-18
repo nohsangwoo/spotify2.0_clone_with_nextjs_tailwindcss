d1065774fceedf1ddce0116a99cde5891ec327a9
nohsangwoo <nsgr12@naver.com>
2022-01-18 11:31:32 +0900

    ch directory

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,13 +3,11 @@ import Sidebar from '../components/Sidebar'
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="bg-black h-screen overflow-hidden">
       <Head>
         <title>spotify 2.0</title>
         {/* <link rel="icon" href="/favicon.ico" /> */}
       </Head>
-      <h1>this is a DOPE spotify 2.0 build</h1>
-
       <main>
         <Sidebar />
         {/* Center */}
