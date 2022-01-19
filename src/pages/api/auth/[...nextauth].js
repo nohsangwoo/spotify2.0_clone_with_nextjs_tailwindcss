2f4b2d5b3deaf678e8de762842a35bda60b4a448
nohsangwoo <nsgr12@naver.com>
2022-01-19 14:13:39 +0900

    redirect to login page when browser's dosen't have a token

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -62,6 +62,7 @@ export default NextAuth({
 
       // Return previous token if the access token has not expired yet
       if (Date.now() < token.accessTokenExpires) {
+        console.log('EXISTING ACCESS TOKEN IS VALID')
         return token
       }
 
