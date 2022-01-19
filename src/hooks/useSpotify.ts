f1082662d015f61717b97baf5359e8e9f03de863
nohsangwoo <nsgr12@naver.com>
2022-01-19 17:43:41 +0900

    useSpotify

---
+++ src/hooks/useSpotify.ts
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from 'react'
+import { useSession, signIn } from 'next-auth/react'
+const SpotifyWebApi = require('spotify-web-api-node/src/spotify-web-api')
+
+const spotifyApi = new SpotifyWebApi({
+  clientId: process.env.NEXT_PUBLIC_CLIENT_ID,
+  clientSecret: process.env.NEXT_PUBLIC_CLIENT_SECRET,
+})
+
+const useSpotify = () => {
+  const { data: session, status } = useSession()
+  useEffect(() => {
+    if (session) {
+      // If refresh access token attempt fails, direct user to login...
+      if (session.error === 'refreshAccessTokenError') {
+        signIn()
+      }
+
+      spotifyApi.setAccessToken(session.accessToken)
+    }
+  }, [session])
+  return spotifyApi
+}
+
+export default useSpotify
