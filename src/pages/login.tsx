485e173e10b143eaaae7275f25dfe19747674c5f
nohsangwoo <nsgr12@naver.com>
2022-01-21 14:05:58 +0900

    ch

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -12,18 +12,18 @@ interface Props {
     }
   }
 }
-const Login = ({ providers }: Props) => {
+const Login = (props: Props) => {
   return (
     <div className="flex flex-col items-center bg-black min-h-screen w-full justify-center">
       <img className="w-52 mb-5" src="https://links.papareact.com/9xl" alt="" />
-      {Object.values(providers).map(provider => {
+      {Object.values(props?.providers).map(provider => {
         return (
-          <div key={provider.name}>
+          <div key={provider?.name}>
             <button
               className="bg-[#18D860] text-white p-5 rounded-full"
               onClick={() => signIn(provider.id, { callbackUrl: '/' })}
             >
-              Login with {provider.name}
+              Login with {provider?.name}
             </button>
           </div>
         )
@@ -34,12 +34,12 @@ const Login = ({ providers }: Props) => {
 
 export default Login
 
-// export async function getServerSideProps() {
-//   const providers = await getProviders()
+export async function getServerSideProps() {
+  const providers = await getProviders()
 
-//   return {
-//     props: {
-//       providers,
-//     },
-//   }
-// }
+  return {
+    props: {
+      providers,
+    },
+  }
+}
